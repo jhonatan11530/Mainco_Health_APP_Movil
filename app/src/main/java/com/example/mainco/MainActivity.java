57a1b80ee119226b8daac725af19c22e25cc5021
jhonatan1153 <jhonatan1153@hotmail.com>
2019-12-09 14:00:36 -0500

    ok

--- app/src/main/java/com/example/mainco/MainActivity.java
+++ app/src/main/java/com/example/mainco/MainActivity.java
@@ -5,13 +5,15 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int DURACION = 2000;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +24,42 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        final ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
 
-
-
-        new Handler().postDelayed(new Runnable() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            };
-        },DURACION);
+                while(progressStatus < 100){
+                    progressStatus +=1;
+
+                    try{
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            pb.setProgress(progressStatus);
+
+                            if(progressStatus == 100){
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    };
+                                },0);
+                            }
+
+                        }
+                    });
+                }
+            }
+        }).start();
+
 
 
     }
--- app/src/main/java/com/example/mainco/options.java
+++ app/src/main/java/com/example/mainco/options.java
@@ -63,140 +63,140 @@ public class options extends AppCompatActivity {
     }
     public void masinfo(View v){
 
-       runOnUiThread( new Runnable() {
-           @Override
-           public void run() {
+        runOnUiThread( new Runnable() {
+            @Override
+            public void run() {
 
 
 
 
-                       View tutorial = getLayoutInflater().inflate(R.layout.tutorial,null);
-                       androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(options.this);
-               builder.setTitle( "TUTORIALES" );
-                       componentes = tutorial.findViewById( R.id.tutorial );
-                       final ArrayList<HELPCOMP> comphelp= new ArrayList<HELPCOMP>();
-                       comphelp.add(new HELPCOMP( "INTRODUCCION A MAINCO APP" ));
-                       comphelp.add(new HELPCOMP( "COMO CREO UN USUARIO ?" ));
-                       comphelp.add(new HELPCOMP( "COMO RECUPERO MI CONTRASEÑA ?" ));
-                       comphelp.add(new HELPCOMP( "COMO INGRESO AL SISTEMA ?" ));
-                       comphelp.add(new HELPCOMP( "COMO EMPIEZO A REGISTRAR ?" ));
-                       comphelp.add(new HELPCOMP( "COMO REGISTRO MIS PAUSAS ?" ));
-                       comphelp.add(new HELPCOMP( "COMO SABER SI SE CERRO LA O.P ?" ));
-                       comphelp.add(new HELPCOMP( "COMO SABER SI YA TERMINE ?" ));
+                View tutorial = getLayoutInflater().inflate(R.layout.tutorial,null);
+                androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(options.this);
+                builder.setTitle( "TUTORIALES" );
+                componentes = tutorial.findViewById( R.id.tutorial );
+                final ArrayList<HELPCOMP> comphelp= new ArrayList<HELPCOMP>();
+                comphelp.add(new HELPCOMP( "INTRODUCCION A MAINCO APP" ));
+                comphelp.add(new HELPCOMP( "COMO CREO UN USUARIO ?" ));
+                comphelp.add(new HELPCOMP( "COMO RECUPERO MI CONTRASEÑA ?" ));
+                comphelp.add(new HELPCOMP( "COMO INGRESO AL SISTEMA ?" ));
+                comphelp.add(new HELPCOMP( "COMO EMPIEZO A REGISTRAR ?" ));
+                comphelp.add(new HELPCOMP( "COMO REGISTRO MIS PAUSAS ?" ));
+                comphelp.add(new HELPCOMP( "COMO SABER SI SE CERRO LA O.P ?" ));
+                comphelp.add(new HELPCOMP( "COMO SABER SI YA TERMINE ?" ));
 
-                       ArrayAdapter<HELPCOMP> adapdtador = new ArrayAdapter<HELPCOMP>( options.this,android.R.layout.simple_dropdown_item_1line,comphelp );
-                       componentes.setAdapter( adapdtador );
+                ArrayAdapter<HELPCOMP> adapdtador = new ArrayAdapter<HELPCOMP>( options.this,android.R.layout.simple_dropdown_item_1line,comphelp );
+                componentes.setAdapter( adapdtador );
 
-                       componentes.setOnItemClickListener( new AdapterView.OnItemClickListener() {
-                           @Override
-                           public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                componentes.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
-                               HELPCOMP H =comphelp.get( position );
+                        HELPCOMP H =comphelp.get( position );
 
-                               if (H.getNombre() == "INTRODUCCION A MAINCO APP"){
+                        if (H.getNombre() == "INTRODUCCION A MAINCO APP"){
 
-                                   Intent e = new Intent(getApplicationContext(), INTRODUCCION.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), INTRODUCCION.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO CREO UN USUARIO ?"){
+                        else if (H.getNombre() == "COMO CREO UN USUARIO ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), CREAR_USER.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), CREAR_USER.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO RECUPERO MI CONTRASEÑA ?"){
+                        else if (H.getNombre() == "COMO RECUPERO MI CONTRASEÑA ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), RECUPERAR_PASSWORD.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), RECUPERAR_PASSWORD.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO INGRESO AL SISTEMA ?"){
+                        else if (H.getNombre() == "COMO INGRESO AL SISTEMA ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), SYSTEM.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), SYSTEM.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO EMPIEZO A REGISTRAR ?"){
+                        else if (H.getNombre() == "COMO EMPIEZO A REGISTRAR ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), REGISTRO_PRODUCIDO.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), REGISTRO_PRODUCIDO.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO REGISTRO MIS PAUSAS ?"){
+                        else if (H.getNombre() == "COMO REGISTRO MIS PAUSAS ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), PAUSE_ACTIVAS.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), PAUSE_ACTIVAS.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO SABER SI SE CERRO LA O.P ?"){
+                        else if (H.getNombre() == "COMO SABER SI SE CERRO LA O.P ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), CLOSE_OP.class);
-                                   startActivity(e);
-                               }
+                            Intent e = new Intent(getApplicationContext(), CLOSE_OP.class);
+                            startActivity(e);
+                        }
 
-                               else if (H.getNombre() == "COMO SABER SI YA TERMINE ?"){
+                        else if (H.getNombre() == "COMO SABER SI YA TERMINE ?"){
 
-                                   Intent e = new Intent(getApplicationContext(), TERMINADO.class);
-                                   startActivity(e);
-                               }
-                           }
-                       } );
+                            Intent e = new Intent(getApplicationContext(), TERMINADO.class);
+                            startActivity(e);
+                        }
+                    }
+                } );
 
-                       builder.setPositiveButton( "VOLVER", new DialogInterface.OnClickListener() {
-                           @Override
-                           public void onClick(DialogInterface dialogInterface, int i) {
+                builder.setPositiveButton( "VOLVER", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                           }
-                       } );
-                       builder.setView( tutorial );
-                       builder.create().show();
-
-
-           }
-       } );
+                    }
+                } );
+                builder.setView( tutorial );
+                builder.create().show();
 
 
             }
-
-     public void support(View v){
-         androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(options.this);
-         builder.setTitle( "CONTACTO" );
-         builder.setMessage( "si desea soporte o comunicarte con el desarrollador de la aplicacion Mainco Health Care.\n\n" +
-                 "INFORMACION DEL DESARROLLADOR\n\n" +
-                 "NOMBRE : JHONATAN FERNANDEZ MUÑOZ \n\n" +
-                 "TELEFONO : 3114360830\n\n" +
-                 "CORREO : jhonatan1153@hotmail.com" );
-         builder.setPositiveButton( "aceptar", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialogInterface, int i) {
-
-             }
-         } );
-         builder.create().show();
-     }
-
-     public void info(View v){
-
-         androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder( options.this );
-         builder.setTitle( "APLICACION MAINCO HEALTH CARE" );
-         builder.setMessage( "USO : " +
-                 "La aplicacion Mainco Health care es una aplicacion de registro de informacion en el area de planta atravez de esta app se podra registrar la hora de entrada , salida , las cantidades produccidas las cantidades defectuosa , el registro de tiempo de descanso \n\n" +
-                 "EXCLUSIVIDAD : " +
-                 "La aplicacion Mainco Health care para dispositivos android es de uso exclusivo para la empresa MAINCO HEALTH CARE se prohibe el uso de esta app por personas (TERCEROS) sin previa autorizacion de MAINCO HEALTH CARE el uso no autorizado de esta app por terceros puede ser sancionado por la respectiva empresa \n\n"
-         );
+        } );
 
 
+    }
 
-          builder.setPositiveButton( "aceptar", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialogInterface, int i) {
+    public void support(View v){
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(options.this);
+        builder.setTitle( "CONTACTO" );
+        builder.setMessage( "si desea soporte o comunicarte con el desarrollador de la aplicacion Mainco Health Care.\n\n" +
+                "INFORMACION DEL DESARROLLADOR\n\n" +
+                "NOMBRE : JHONATAN FERNANDEZ MUÑOZ \n\n" +
+                "TELEFONO : 3114360830\n\n" +
+                "CORREO : jhonatan1153@hotmail.com" );
+        builder.setPositiveButton( "aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
-                     }
-                 } );
-         builder.create().show();
+            }
+        } );
+        builder.create().show();
+    }
 
-     }
+    public void info(View v){
+
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder( options.this );
+        builder.setTitle( "APLICACION MAINCO HEALTH CARE" );
+        builder.setMessage( "USO : " +
+                "La aplicacion Mainco Health care es una aplicacion de registro de informacion en el area de planta atravez de esta app se podra registrar la hora de entrada , salida , las cantidades produccidas las cantidades defectuosa , el registro de tiempo de descanso \n\n" +
+                "EXCLUSIVIDAD : " +
+                "La aplicacion Mainco Health care para dispositivos android es de uso exclusivo para la empresa MAINCO HEALTH CARE se prohibe el uso de esta app por personas (TERCEROS) sin previa autorizacion de MAINCO HEALTH CARE el uso no autorizado de esta app por terceros puede ser sancionado por la respectiva empresa \n\n"
+        );
 
 
-        }
+
+        builder.setPositiveButton( "aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        } );
+        builder.create().show();
+
+    }
+
+
+}
