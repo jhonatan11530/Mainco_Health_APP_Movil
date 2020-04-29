b0dc01599eef525f76dc3a47071127c988d9aae2
jhonatan11530 <56049236+jhonatan11530@users.noreply.github.com>
2020-04-29 11:52:20 -0500

    ok

---
+++ validar/actualizaEntrada.php
@@ -0,0 +1,12 @@
+<?php
+$ID = $_GET["id"];
+$op = $_GET["op"];
+$inicial = $_GET["Finicial"];
+$inicialh = $_GET["Hinicial"];
+
+
+$mysqli = mysqli_connect("127.0.0.1", "root", "", "proyecto");
+ $sql_statement = "UPDATE IGNORE operador SET inicial='".$inicial."',hora_inicial='".$inicialh."',numero_op='".$op."' WHERE id= '".$ID."'";
+  $result = mysqli_query($mysqli, $sql_statement);
+
+?>
