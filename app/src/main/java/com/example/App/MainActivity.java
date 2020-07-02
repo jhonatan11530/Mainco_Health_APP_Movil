8432b6d195b65098bbeb43a4cdd987b7d795612f
jhonatan1153 <jhonatan1153@hotmail.com>
2020-07-02 14:01:20 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/App/MainActivity.java
+++ app/src/main/java/com/example/App/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
+
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
