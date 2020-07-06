1a6cbf05b097e8afb1e5cd8c0406440d6cacfb36
jhonatan1153 <jhonatan1153@hotmail.com>
2020-07-06 15:55:53 -0500

    falta agregar a services consolidado
    ok

--- app/src/main/java/com/example/App/OperadorActivity.java
+++ app/src/main/java/com/example/App/OperadorActivity.java
@@ -587,14 +587,7 @@ public class OperadorActivity extends AppCompatActivity {
                                 builder.setPositiveButton("FINALIZAR O.P", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                        Toast.makeText(getApplicationContext(),"EL NOMBRE ES : "+ NOMBRE.toString(),Toast.LENGTH_SHORT).show();
-                                        /* new Thread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                HttpRequest.get("http://" + cambiarIP.ip + "/validar/consolidado.php?nombre=" + NOMBRE.toString()).body();
-                                            }
-                                        }).start();*/
-
+                                        HttpRequest.get("http://" + cambiarIP.ip + "/validar/consolidado.php?nombre="+NOMBRE.toString()).body();
                                     }
                                 });
                                 builder.setNegativeButton("CONTINUAR ACTIVIDAD", new DialogInterface.OnClickListener() {
