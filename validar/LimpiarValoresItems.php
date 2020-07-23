98d7b8383991a8146c27267cb7235d3f1b6ed6e1
jhonatan1153 <jhonatan1153@hotmail.com>
2020-07-23 11:51:35 -0500

    ACTUALIZADO A SQL SERVER

--- validar/LimpiarValoresItems.php
+++
@@ -1,9 +0,0 @@
-<?php
- $op = $_GET["op"];
- $totales = 0;
- $mysqli = mysqli_connect("127.0.0.1", "root", "", "proyecto");
- $res = "UPDATE tarea SET cantidadpentiente='". $totales."' WHERE numero_op = '".$op."' ";
- 
- $result = mysqli_query($mysqli, $res);
- 
-?>
