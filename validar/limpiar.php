2f41b1c0f3643fced2f9cfebc028b9d2884acd99
jhonatan1153 <jhonatan1153@hotmail.com>
2020-08-20 15:01:25 -0500

    ok

--- validar/limpiar.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("ConexionSQL.php");
-$limpio = $_REQUEST["id"];
-$cero = 0;
-$mysqli = sqlsrv_connect(Server() , connectionInfo());
-  $sql_statement = "UPDATE proyecto.tarea SET cantidadpentiente= '".$cero."' WHERE  numero_op = '".$numeroops."' ";
-  $resultado = sqlsrv_query($mysqli, $sql_statement);
-?>
