f4bae40461cb65e235c924c5399ef83948e0e3c3
jhonatan11530 <56049236+jhonatan11530@users.noreply.github.com>
2020-11-09 15:53:51 -0500

    ok

--- app/src/main/java/com/Mainco/App/OperadorActivity.java
+++ app/src/main/java/com/Mainco/App/OperadorActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
